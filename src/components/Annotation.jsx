e54bc46d34254b54606c9de5a6c1f651cc2267b7
RM <RRMoelker@users.noreply.github.com>
2019-05-14 09:53:09 +0200

    Annotation tool up and running

--- src/components/Annotation.jsx
+++ src/components/Annotation.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Anntation = ({item}) => {
+const Annotation = ({item}) => {
   // console.log(item);
-  // const url = item.iiif_url || 'https://www.amsterdam.nl/publish/pages/858225/logo.png';
-  const url = 'https://www.amsterdam.nl/publish/pages/858225/logo.png';
+  const url = item.iiif_url || 'https://www.amsterdam.nl/publish/pages/858225/logo.png';
+  // const url = 'https://www.amsterdam.nl/publish/pages/858225/logo.png';
 
-  return <img src={url} alt='item to label' class="full-img"/>
+  return <img src={url} alt='item to label' className="full-img"/>
 };
 
-export default Anntation;
+export default Annotation;
--- src/components/Annotator.jsx
+++ src/components/Annotator.jsx
@@ -3,6 +3,22 @@ import Annotation from "./Annotation";
 
 const API_ROOT = 'http://localhost:5000/';
 
+const availableTypes = [
+  'aanvraag',
+  'besluit',
+  'correspondentie',
+  'aanpassing',
+  'aanvraag revisie',
+  'bijlage',
+  'bijlage aanvraag',
+  'bijlage vergunning',
+  'bijlage beschikking',
+  'aansluitvoorschriften',
+  'vergunning',
+  'zienswijze',
+  // 'bijlage bij beschikking',
+];
+
 const getAnnotationList = () => {
   const url = API_ROOT;
   return fetch(url)
@@ -12,7 +28,6 @@ const getAnnotationList = () => {
 const putAnnotation = (index, value) => {
   const url = `${API_ROOT}put/${index}`;
   const data = JSON.stringify({ "document_type": value});
-  console.log(data);
   return fetch(url, {
     headers: {
       'Accept': 'application/json',
@@ -44,6 +59,9 @@ class Annotator extends React.Component {
   }
 
   _updateType(newValue) {
+    if (newValue.length > 0 && availableTypes.indexOf(newValue) < 0) {
+      throw new Error(`unkown type!: ${newValue}`);
+    }
     const { items, currentIndex } = this.state;
     putAnnotation(currentIndex, newValue).then(() => {
       const item = items[currentIndex];
@@ -53,13 +71,39 @@ class Annotator extends React.Component {
     }).catch(e => console.error(e));
   }
 
+  _changeType(currentValue, direction) {
+    const idx = availableTypes.indexOf(currentValue);
+    if (idx >= 0) {
+      const type = availableTypes[(availableTypes.length + idx + direction) % availableTypes.length];
+      this._updateType(type)
+    } else {
+      this._updateType(availableTypes[0])
+    }
+  }
+
+  _onIndexChange(event) {
+    const value = event.target.value;
+
+    if (value >= 0 && value < this.state.items.length) {
+      this.setState({ currentIndex: value });
+    }
+  }
+
   componentDidMount() {
     getAnnotationList().then(items => {
-      const currentIndex = 800;
+      const currentIndex = 600;
       this.setState({
         items,
         currentIndex,
       })
+    });
+
+    document.addEventListener('keydown', (event) => {
+      const key = event.key || event.keyCode;
+      if (key === ' ') {
+        event.preventDefault();
+        return false; // Prevent page scroll from space key
+      }
     });
 
     document.addEventListener('keyup', (event) => {
@@ -69,7 +113,8 @@ class Annotator extends React.Component {
 
       const key = event.key || event.keyCode;
 
-      const { items } = this.state;
+      const { items, currentIndex } = this.state;
+      const item = items && items[currentIndex];
 
       if (key === 'Enter') {
         console.log('saving');
@@ -82,12 +127,32 @@ class Annotator extends React.Component {
         this.setState({ currentIndex: Math.min(this.state.currentIndex + 1, items.length) })
       }
 
-      if (key === 'a') {
-        this._updateType('aanvraag');
-      }
+      if (item) {
+        if (key === 'Escape') {
+          this._updateType('');
+        }
+        if (key === '+') {
+          this._changeType(item.document_type, 1);
+        }
 
-      if (key === 'b') {
-        this._updateType('besluit');
+        if (key === '-') {
+          this._changeType(item.document_type, -1);
+        }
+
+        if (key === 'a') {
+          this._updateType('aanvraag');
+        }
+
+        if (key === 'b') {
+          this._updateType('besluit');
+        }
+
+        let keyCode = event.keyCode;
+        if (keyCode >= 49 && keyCode <= 57) {
+          // numerical 1 through 9
+          const idx = keyCode - 49; // key 1 -> index 0
+          this._updateType(availableTypes[idx]);
+        }
       }
 
     });
@@ -99,7 +164,7 @@ class Annotator extends React.Component {
     return <div>
       <div className="overlay">
         <ul>
-          <li><span>Current </span><span>{currentIndex}</span></li>
+          <li><span>Current </span><input type="number" min="0" value={currentIndex} onChange={this._onIndexChange.bind(this)} /></li>
           <li><span>Total </span><span>{items && items.length}</span></li>
           <li><span>Document_type </span><span>{item && item.document_type}</span></li>
         </ul>
