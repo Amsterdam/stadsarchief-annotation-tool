0aa29820f79aa8aeb80f5231b28d0b6b7c9ce0d7
RM <RRMoelker@users.noreply.github.com>
2019-08-19 15:10:24 +0200

    react router basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
-import Annotator from "./components/Annotator";
+import Annotator from './components/Annotator';
+
+
+function Index() {
+  return <h2>Home</h2>;
+}
 
 function App() {
   return (
-    <Annotator/>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/annotator/">Annotator</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/annotator/" component={Annotator} />
+      </div>
+    </Router>
+
   );
 }
 
